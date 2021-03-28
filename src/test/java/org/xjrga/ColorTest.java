d4c9b4fe70560e0a48ddcc4f3fe21c781fe7dbe2
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2021-03-28 18:06:52 -0500

    wip

--- src/test/java/org/xjrga/ColorTest.java
+++ src/test/java/org/xjrga/ColorTest.java
@@ -1,5 +1,6 @@
 package org.xjrga;
 
+import java.awt.Color;
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -25,5 +26,25 @@ public class ColorTest
     public void testBlue() {
         MyColor color = new MyColor(37,52,4);
         Assert.assertEquals(4, color.getBlue());
+    }
+    
+    @Test
+    public void testBrighterColor() {
+        //5e850a -> 85bd0d
+        MyColor color = new MyColor(37,52,4);                
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {            
+            sb.append(color.brighter().getRed());
+            sb.append(",");
+            sb.append(color.brighter().getGreen());
+            sb.append(",");
+            sb.append(color.brighter().getBlue());
+            sb.append("\n");            
+        }
+        System.out.println(sb.toString());
+        
+        //Assert.assertEquals(37, brighterColor.getRed());
+        Assert.assertFalse(true);        
+        
     }
 }
