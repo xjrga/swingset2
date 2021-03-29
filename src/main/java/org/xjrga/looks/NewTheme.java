5f92171108142f8824e26cd912f3555c62a69559
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2021-03-28 22:38:55 -0500

    wip

--- src/main/java/org/xjrga/looks/NewTheme.java
+++ src/main/java/org/xjrga/looks/NewTheme.java
@@ -33,35 +33,35 @@ public class NewTheme extends MetalTheme {
         //font = new FontUIResource(TheFonts.getFreeMono(Float.valueOf(13)));        
         //font = new FontUIResource(TheFonts.getUbuntuMono(Float.valueOf(13)));
 
-        secondary3 = new ColorUIResource(color.get00());
+        secondary3 = new ColorUIResource(color.get40());
         //app, menu background, selected tab
 
-        secondary2 = new ColorUIResource(color.get15());
+        secondary2 = new ColorUIResource(color.get35());
         //unselected tab, iframes side lines,scrollbar outline
         //slider outline and ticks,pressed menuitem,table grid, menubar outline
         //slider disabled ticks, slider internal track, titled border outline,
         //pressed button, selected toolbar button, filechooser file background,
         //disabled text
 
-        primary1 = new ColorUIResource(color.get10());
+        primary1 = new ColorUIResource(color.get30());
         //scrollbar knob outline and dots, folder tab, 
         //selected iframe outline and dots, tooltip outline,
         //slider knob dots
 
-        secondary1 = new ColorUIResource(color.get10());
+        secondary1 = new ColorUIResource(color.get30());
         //iframe outlines, button outlines, menu outline, menu item checkbox, 
         //textbox outline, component outlines
 
-        primary2 = new ColorUIResource(color.get05());//
+        primary2 = new ColorUIResource(color.get20());//
         //internal frame background, component focus
         //menu item background, selected scrollbar knob background, folders line
         //slider ticks, selected tree item outline
 
-        primary3 = new ColorUIResource(color.get00());
+        primary3 = new ColorUIResource(color.get10());
         //selected window, selected menuitem box, tree component
         //selected window top bar, scrollbar knob left outline, tooltip, 
         //selected list item, selected slider knob highlight,
-        //folders, table row selection, selected tree item        
+        //folders, table row selection, selected tree item    
     }
 
     @Override
